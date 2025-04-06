22c06424d79dc9500eb06e3f4e6625fec85408c7
fcisco-home <srfrancisco17@gmail.com>
2025-04-06 13:15:45 -0500

    Ep 16 - Forms and CSRF Explained (with Examples)

--- routes/web.php
+++ routes/web.php
@@ -10,27 +10,34 @@ Route::get('/', function () {
 
 Route::get('/jobs', function () {
 
-    $jobs = Job::with('employer')->paginate(3);
+    $jobs = Job::with('employer')->latest()->paginate(10);
 
-    return view('jobs', [
+    return view('jobs.index', [
         'jobs' => $jobs
     ]);
 });
 
-Route::get('/contact', function () {
-    return view('contact');
+Route::get('/jobs/create', function (){
+    return view('jobs.create');
 });
 
 Route::get('/jobs/{id}', function ($id){
-
-    /*
-    Arr::first($jobs, function($job) use ($id){
-        return $job['id'] == $id;
-    });
-    */
-
     $job = Job::find($id);
+    return view('jobs.show', ['job' => $job]);
+});
 
+Route::post('/jobs', function (){
 
-    return view('job', ['job' => $job]);
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => '1',
+    ]);
+
+    return redirect('/jobs');
+
+});
+
+Route::get('/contact', function () {
+    return view('contact');
 });
